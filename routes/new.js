c09efa9e3f3040465e182062efa863c0708ed343
divineLush <alvipath@yahoo.com>
2024-04-24 17:23:12 +0700

    displaying author only to authenticated users

--- routes/new.js
+++ routes/new.js
@@ -12,7 +12,8 @@ router.get('/new', (req, res, next) => {
 router.post('/new', passport.authenticate('jwt', { session: false, failureRedirect: '/' }), async (req, res, next) => {
   try {
     const date = `${new Date().getDay()}/${new Date().getMonth()}/${new Date().getFullYear()}`;
-    await (await db).collection('posts').insertMany([{ title: req.body.title, post: req.body.post, date }]);
+    const user = jwt.decode(req.cookies.jwt);
+    await (await db).collection('posts').insertMany([{ title: req.body.title, post: req.body.post, date, user }]);
     res.redirect('/');
   } catch(err) {
     next(err);
