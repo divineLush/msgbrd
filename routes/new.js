6963667a39b5e2ae15398299700c43d0ff8ed962
divineLush <alvipath@yahoo.com>
2024-04-24 17:14:22 +0700

    redirect on authentication fail

--- routes/new.js
+++ routes/new.js
@@ -9,7 +9,7 @@ router.get('/new', (req, res, next) => {
   res.render('new', { user });
 });
 
-router.post('/new', passport.authenticate('jwt', { session: false }), async (req, res, next) => {
+router.post('/new', passport.authenticate('jwt', { session: false, failureRedirect: '/' }), async (req, res, next) => {
   try {
     const date = `${new Date().getDay()}/${new Date().getMonth()}/${new Date().getFullYear()}`;
     await (await db).collection('posts').insertMany([{ title: req.body.title, post: req.body.post, date }]);
